3fdf44af10ea14eae5202710570c308cd7d828fb
kurosio <harborwolf1@gmail.com>
2020-11-21 16:38:28 +0300

    var unused

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -320,7 +320,7 @@ void CCharacter::FireWeapon()
 
 			CBuilding* apBuildings[1];
 			CCharacter *apEnts[MAX_CLIENTS];
-			int Hits = 0;
+			// int Hits = 0;
 			int Num = GameServer()->m_World.FindEntities(m_Pos, GetProximityRadius(), (CEntity**)apBuildings, 1, CGameWorld::ENTTYPE_BUILDING);
 			int Num2 = GameServer()->m_World.FindEntities(ProjStartPos, GetProximityRadius() * 0.5f, (CEntity**)apEnts, MAX_CLIENTS, CGameWorld::ENTTYPE_CHARACTER);
 
